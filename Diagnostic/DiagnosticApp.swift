9c2d08d3a36204e01eb1feac591690565604d749
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2025-12-15 08:42:09 -0800

    Initial Commit

---
+++ Diagnostic/DiagnosticApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiagnosticApp.swift
+//  Diagnostic
+//
+//  Created by Dylan J. Widmayer on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DiagnosticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
