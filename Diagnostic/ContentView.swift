01bc81d93a3961550de04e2430ef238b94ce9271
Nicholas Wiesneth <nwiesneth8478@stu.d214.org>
2025-12-17 10:34:11 -0600

    comit

--- Diagnostic/ContentView.swift
+++ Diagnostic/ContentView.swift
@@ -14,6 +14,11 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .foregroundStyle(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+            
         }
     }
 }
