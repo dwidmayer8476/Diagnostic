f07ed42ce813e6f9c26c26c040a461760b46671a
Nicholas Wiesneth <nwiesneth8478@stu.d214.org>
2025-12-17 10:25:05 -0600

    ha

--- Diagnostic/ContentView.swift
+++ Diagnostic/ContentView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        ZStack {
+            Image("BackGround")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("test")
         }
-        .padding()
     }
 }
 
