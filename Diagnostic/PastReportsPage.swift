d036979560dcca2638cd8bbbd98073e35fc80175
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2026-02-23 08:21:28 -0800

    made page for past reports

---
+++ Diagnostic/PastReportsPage.swift
@@ -0,0 +1,14 @@
+//
+//  Untitled.swift
+//  Diagnostic
+//
+//  Created by Dylan J. Widmayer on 2/23/26.
+//
+import SwiftUI
+
+struct PastReportsPage: View {
+    
+    var body: some View {
+  
+    }
+}
